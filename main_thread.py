d1b5bbc5a29afe25dd7435551480019824d9a462
MrAnyx <1ptitigre@gmail.com>
2021-09-20 17:55:48 -0400

    Feat: Added thread script

---
+++ main_thread.py
@@ -0,0 +1,43 @@
+from objects.Board import Board
+from objects.Window import Window
+from objects.Robot import Robot
+from threading import *
+import time
+
+# Todo: Commenter le code
+
+b = Board()
+r = Robot(b)
+w = Window(500, 500, False, "TP1 IA", b, r)
+
+# _counter = 1
+
+def update_env():
+   global r, b
+   while(r.energy > 0):
+      b.random_dust_jewel()
+      time.sleep(1)
+
+def update_robot():
+   global r, b
+   while(r.energy > 0):
+      goal = r.select_nearest_not_empty_room()
+      r.reach_selected_room(goal)
+      
+      if(r.is_on_goal(goal)):
+         r.clean_or_take(goal)
+
+      r.display_current_energy()
+      time.sleep(1)
+
+t2 = Thread(target=update_robot, args=[])
+t1 = Thread(target=update_env, args=[])
+
+t2.start()
+t1.start()
+
+while(r.energy > 0):
+   w.display_board()
+   time.sleep(1)
+
+w.mainloop()

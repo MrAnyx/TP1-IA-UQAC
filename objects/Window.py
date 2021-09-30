7f72966935aae06d64045b81537e413660f61063
MrAnyx <1ptitigre@gmail.com>
2021-09-30 17:29:33 -0400

    Fix: Removed useless function

--- objects/Window.py
+++ objects/Window.py
@@ -36,46 +36,6 @@ class Window:
     def update(self):
         self.window.update()
 
-    # TODO Mettre cette fonction dans une classe à part pour l'appeler en static de partout
-    # TODO Mettre le board et le robot en paramètre
-    def display_board(self):
-        # self.canvas.delete("all")
-        color = None
-        for i in range(5):
-            for j in range(5):
-                if self.board.get_board()[i][j] == 0:
-                    color = "#f3f3f3"  # Rien
-                elif self.board.get_board()[i][j] == 1:
-                    color = "#2288ba"  # Bijoux
-                elif self.board.get_board()[i][j] == 2:
-                    color = "#865f3e"  # Poussière
-                elif self.board.get_board()[i][j] == 3:
-                    color = "#8eba4f"  # Poussière + Bijoux
-
-                _rect = self.canvas.create_rectangle(
-                    i * 100, j * 100, (i + 1) * 100, (j + 1) * 100, fill=color
-                )
-
-                _text = self.canvas.create_text(
-                    i * 100 + 50,
-                    j * 100 + 50,
-                    text=i + 5 * j,
-                    fill="black",
-                    font=("Helivica 13 bold"),
-                )
-
-        # On affiche le robot
-        _rect = self.canvas.create_rectangle(
-            self.robot.x * 100 + 25,
-            self.robot.y * 100 + 25,
-            (self.robot.x * 100) + 75,
-            (self.robot.y * 100) + 75,
-            fill="#888",
-        )
-
-        # On update le canvas pour que les nouvelles modifs soient prises en compte
-        self.update()
-
     def get_window(self):
         return self.window
 
